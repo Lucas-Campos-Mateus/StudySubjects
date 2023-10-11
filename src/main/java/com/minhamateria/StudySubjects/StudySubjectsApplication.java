3c04e864725cfdeb6d6b00f8f85cc2ee4c52e9a6
Lucas Campos <iphonelucas___@outlook.com>
2023-10-11 20:41:04 -0300

    First commit

---
+++ src/main/java/com/minhamateria/StudySubjects/StudySubjectsApplication.java
@@ -0,0 +1,13 @@
+package com.minhamateria.StudySubjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySubjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySubjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhamateria/StudySubjects/StudySubjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhamateria.StudySubjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySubjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
